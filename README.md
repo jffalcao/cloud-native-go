1618eed637b3337c183e84d399e95a6135785c24
Jose Fernando Falcao <jfribeirofalcao@gmail.com>
2018-02-16 06:35:05 -0500

    Variable port dependent on env var

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cloud-native-go
 Packt Video: Getting Started with Cloud Native Go
+
+Clone the repository:
+
+`$ git clone https://github.com/jffalcao/cloud-native-go.git`
---
+++ microservice.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Hello Cloud Native Go.")
+}
